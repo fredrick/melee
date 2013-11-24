8236cf15b078e31dae8bb98a61ecf98e87ea5145
Fredrick Galoso <fredrickgaloso@gmail.com>
2013-11-24 17:14:10 -0600

    Look up a node to use for an object

--- src/melee/partitioning.clj
+++ src/melee/partitioning.clj
@@ -4,7 +4,7 @@
 (defprotocol Partitioner
   (add [this node])
   (delete [this node])
-  (lookup [this node]))
+  (lookup [this object]))
 
 ;;; Consistent hashing
 
@@ -12,19 +12,36 @@
   (map #(hash-map (hash->int (hash-object (murmur3-32 %) node)) node)
     (range replicas)))
 
-(defn- ring-push [node replicas ring]
-  (apply merge ring (consistent-partition node replicas)))
+(defn- ring-push [ring node replicas]
+  (apply merge ring (allocate node replicas)))
 
-(defn- ring-pop [node replicas ring]
+(defn- ring-pop [ring node replicas]
   (apply dissoc ring (map first (map keys (allocate node replicas)))))
+
+(defn- tail-map [ring hash]
+  (filter #(<= 0 (compare (key %) hash)) ring))
 
 (defrecord ConsistentHash [replicas ring]
   Partitioner
   (add [this node]
-    (ConsistentHash. replicas (ring-push node replicas ring)))
+    "Add node to consistent hash ring."
+    (ConsistentHash. replicas (ring-push ring node replicas)))
   (delete [this node]
-    (ConsistentHash. replicas (ring-pop node replicas ring)))
-  (lookup [this node]))
+    "Delete node from consistent hash ring."
+    (ConsistentHash. replicas (ring-pop ring node replicas)))
+  (lookup [this object]
+    "Lookup node to use for object from consistent hash ring."
+    (if-not (empty? ring)
+      (let [hash (hash->int (hash-object (murmur3-32) object))
+            tail-map (tail-map ring hash)]
+        (if (empty? tail-map)
+          (val (first ring))
+          (val (first tail-map)))))))
 
-(defn consistent-hash [replicas]
-  (ConsistentHash. replicas (sorted-map)))
+(defn consistent-hash
+  ([replicas]
+    "Create a new consistent hash ring."
+    (ConsistentHash. replicas (sorted-map)))
+  ([nodes replicas]
+    "Create a new consistent hash ring with one or many seed nodes."
+    (reduce add (ConsistentHash. replicas (sorted-map)) nodes)))
--- test/melee/partitioning_test.clj
+++ test/melee/partitioning_test.clj
@@ -4,22 +4,37 @@
   (:use [midje.sweet]))
 
 (facts "Consistent hashing partitioner"
-  (let [ring (consistent-hash 3)
+  (let [ring (consistent-hash 10)
         node1 "node1"
         node2 "node2"
         node3 "node3"]
-    (fact "Add object"
+    (fact "Add node"
       (:ring (add ring node1)) => (has every? #(= node1 %)))
 
-    (fact "Add many objects"
+    (fact "Add many nodes"
       (let [updated-ring (:ring (add (add (add ring node1) node2) node3))]
         updated-ring => (has some #(= node1 %))
         updated-ring => (has some #(= node2 %))
         updated-ring => (has some #(= node3 %))))
 
-    (fact "Remove object"
+    (fact "Remove node"
       (:ring (delete (add ring node1) node1)) => {})
 
-    (fact "Remove many objects"
+    (fact "Remove many nodes"
       (let [full-ring (add (add (add ring node1) node2) node3)]
-        (:ring (delete (delete full-ring node1) node2)) => (has every? #(= node3 %))))))
+        (:ring (delete (delete full-ring node1) node2)) => (has every? #(= node3 %))))
+
+    (fact "Lookup node for empty hash ring"
+      (lookup ring "object") => nil?)
+
+    (fact "Lookup node for hash ring with one node"
+      (lookup (add ring node1) "object1") => node1)
+
+    (fact "Lookup another node for hash ring with two nodes"
+      (lookup (add (add ring node1) node2) "object2") => node2)
+
+    (fact "Lookup another node for hash ring with three nodes"
+      (lookup (add (add (add ring node1) node2) node3) "object5") => node3)
+
+    (fact "Initialize consistent hash with vector of nodes"
+      (consistent-hash [node1 node2 node3] 10) => (add (add (add ring node1) node2) node3))))
