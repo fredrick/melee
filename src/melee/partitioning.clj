0d014739ba8e3b9f3de3f8c5ef158658b42aa511
Fredrick Galoso <fredrickgaloso@gmail.com>
2013-11-24 21:28:07 -0600

    Use 128 bit hashing

--- src/melee/partitioning.clj
+++ src/melee/partitioning.clj
@@ -9,7 +9,7 @@
 ;;; Consistent hashing
 
 (defn- allocate [node replicas]
-  (map #(hash-map (hash->int (hash-object (murmur3-32 %) node)) node)
+  (map #(hash-map (hash->long (hash-object (murmur3-128 %) node)) node)
     (range replicas)))
 
 (defn- ring-push [ring node replicas]
@@ -30,9 +30,9 @@
     "Delete node from consistent hash ring."
     (ConsistentHash. replicas (ring-pop ring node replicas)))
   (lookup [this object]
-    "Lookup node to use for object from consistent hash ring."
+    "Look up node to use for object from consistent hash ring."
     (if-not (empty? ring)
-      (let [hash (hash->int (hash-object (murmur3-32) object))
+      (let [hash (hash->long (hash-object (murmur3-128) object))
             tail-map (tail-map ring hash)]
         (if (empty? tail-map)
           (val (first ring))
