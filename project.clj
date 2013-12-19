dd75d98cf8c3f789c290241025ebade7ef298ed5
Fredrick Galoso <fredrickgaloso@gmail.com>
2013-12-18 21:02:03 -0600

    Remove dependency on thrift-clj

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [thrift-clj "0.2.0"]
                  [com.google.guava/guava "15.0"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                    [org.clojure/tools.nrepl "0.2.3"]
