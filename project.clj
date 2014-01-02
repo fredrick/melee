a9691abd678e0c50bbd72c6b011c61d1ae0bbe1e
Fredrick Galoso <fredrickgaloso@gmail.com>
2014-01-01 22:05:10 -0600

    Update dev dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject melee "0.1.0-SNAPSHOT"
   :description "Distributed coordinator"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/wayoutmind/melee"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,8 +8,8 @@
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                    [org.clojure/tools.nrepl "0.2.3"]
                                    [org.clojure/java.classpath "0.2.1"]
-                                   [midje "1.5.1"]
-                                   [bultitude "0.2.2"]]
+                                   [midje "1.6.0"]
+                                   [bultitude "0.2.4"]]
                     :plugins [[lein-midje "3.1.1"]
                               [lein-marginalia "0.7.1"]]}}
   :global-vars {*warn-on-reflection* true})
