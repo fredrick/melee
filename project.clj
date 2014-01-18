a42bb409c799820c9bade517c1b10777552ecec1
Fredrick Galoso <fredrickgaloso@gmail.com>
2014-01-17 20:21:23 -0600

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.google.guava/guava "15.0"]]
+                 [com.google.guava/guava "16.0"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                    [org.clojure/tools.nrepl "0.2.3"]
-                                   [org.clojure/java.classpath "0.2.1"]
+                                   [org.clojure/java.classpath "0.2.2"]
                                    [midje "1.6.0"]
-                                   [bultitude "0.2.4"]]
+                                   [bultitude "0.2.5"]]
                     :plugins [[lein-midje "3.1.1"]
                               [lein-marginalia "0.7.1"]]}}
   :global-vars {*warn-on-reflection* true})
