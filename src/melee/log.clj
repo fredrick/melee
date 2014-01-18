bc005369ce4521f1903971c5bbfee25322b6178d
Fredrick Galoso <fredrickgaloso@gmail.com>
2014-01-18 13:52:32 -0600

    Wrap state machine log in protocol

--- src/melee/log.clj
+++ src/melee/log.clj
@@ -1,21 +1,33 @@
 (ns melee.log
   (:import (clojure.lang IPersistentVector)))
 
+(defprotocol Log
+  (start-index
+    [this]
+    "Returns first log index.")
+  (last-index
+    [this]
+    "Returns last log index.")
+  (last-term
+    [this]
+    "Returns last log term.")
+  (append!
+    [this {:term :success} entry]
+    "Appends a log entry to the log given append entries response is
+    successful."))
+
+(extend-protocol Log
+  IPersistentVector
+  (start-index [this]
+    (if (empty? this) 0 (:prev-log-index (first this))))
+  (last-index [this]
+    (if (empty? this) 0 (:prev-log-index (last this))))
+  (last-term [this]
+    (if (empty? this) 0 (:term (last this)))))
+
 (defrecord Entry [^Number term leader-id ^Number prev-log-index ^Number prev-log-term entries ^Number leader-commit])
 
 (defn entry
   "Entry for log."
   [^Number term leader-id ^Number prev-log-index ^Number prev-log-term entries ^Number leader-commit]
   (->Entry term leader-id prev-log-index prev-log-term entries leader-commit))
-
-(defn start-index
-  "Returns first log index."
-  [^IPersistentVector log] (if (empty? log) 0 (:prev-log-index (first log))))
-
-(defn last-index
-  "Returns last log index."
-  [^IPersistentVector log] (if (empty? log) 0 (:prev-log-index (last log))))
-
-(defn last-term
-  "Returns last log term."
-  [^IPersistentVector log] (if (empty? log) 0 (:term (last log))))
