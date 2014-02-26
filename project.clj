cf2f6100c36d926085fd315b2f766d733dce9ee5
Fredrick Galoso <fredrickgaloso@gmail.com>
2014-02-25 21:21:10 -0600

    Add core.async for local cluster testing, evaluation of IPC

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.google.guava/guava "16.0.1"]
                  [com.github.sbtourist/journalio "1.4.2"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
