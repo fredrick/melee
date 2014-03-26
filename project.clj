8010f1e3bc09f24a30f39a8c50bff622be27d005
Fredrick Galoso <fredrickgaloso@gmail.com>
2014-03-26 16:05:22 -0500

    Update dependencies, including bump Clojure to 1.6.0

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/wayoutmind/melee"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.google.guava/guava "16.0.1"]
                  [com.github.sbtourist/journalio "1.4.2"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                    [org.clojure/tools.nrepl "0.2.3"]
                                    [org.clojure/java.classpath "0.2.2"]
-                                   [midje "1.6.2"]
-                                   [bultitude "0.2.5"]]
+                                   [midje "1.6.3"]
+                                   [bultitude "0.2.6"]]
                     :plugins [[lein-midje "3.1.1"]
                               [lein-marginalia "0.7.1"]
                               [lein-kibit "0.0.8"]
