694cca1c254a74901d8b4ba1e07f83b63eeaefa0
juergen.schiewe <juergen.schiewe@senacor.com>
2018-05-22 11:42:44 +0200

    Updated project to latest versions of clojure and midje. warn-on-reflection turned off to reduce the noise I cannot avoid anyway

--- project.clj
+++ project.clj
@@ -3,17 +3,18 @@
   :url "https://github.com/wayoutmind/melee"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.google.guava/guava "16.0.1"]
                  [com.github.sbtourist/journalio "1.4.2"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                    [org.clojure/tools.nrepl "0.2.3"]
                                    [org.clojure/java.classpath "0.2.2"]
-                                   [midje "1.6.3"]
+                                   [midje "1.9.1"]
                                    [bultitude "0.2.6"]]
                     :plugins [[lein-midje "3.1.1"]
                               [lein-marginalia "0.7.1"]
                               [lein-kibit "0.0.8"]
                               [lein-bikeshed "0.1.6"]]}}
-  :global-vars {*warn-on-reflection* true})
+  ; :global-vars {*warn-on-reflection* true}
+  )
